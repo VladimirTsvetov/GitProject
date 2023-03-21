3ac23276b1c801d31a9bb17e2b7f5b0a0e796e92
VladimirTsvetov <v.a.tsvetov@gmail.com>
2023-03-21 13:13:37 +0300

    выполнено задание 1

---
+++ src/main/java/lesson1/One.java
@@ -0,0 +1,13 @@
+package lesson1;
+
+public class One {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
